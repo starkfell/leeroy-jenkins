ad0ba698c788fcc8bec2a2ad057910d0527a560f
Ryan Irujo <ryan.irujo@gmail.com>
2016-04-15 07:56:37 +0000

    Adding new node app to repo

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+ 
+var app = express();
+ 
+app.get('/', function (req, res) {
+  res.send('Leeroy Jenkins - first node.js app');
+});
+ 
+app.listen(process.env.PORT || 5000);
+ 
+module.exports = app;
